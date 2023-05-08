284987646f72c41b2ef66ed1dd20d37d0354cb07
patternspandemic <patternspandemic@live.com>
2023-05-08 16:14:39 -0700

    Add utility funcs and procs.

--- src/delaunator.nim
+++ src/delaunator.nim
@@ -8,8 +8,8 @@ var EDGE_STACK: array[512, uint32]
 type
   Delaunator*[T] = ref object
     coords*: seq[T]
-    triangles*: seq[uint32]
-    halfedges*: seq[int32]
+    triangles*: seq[uint32] # trimmed version of d_triangles
+    halfedges*: seq[int32]  # trimmed version of d_halfedges
     hull*: seq[uint32]
 
     # Arrays that will store the triangulation graph
@@ -33,7 +33,130 @@ func defaultGetX[P, T](p: P): T = p[0]
 func defaultGetY[P, T](p: P): T = p[1]
 
 
+proc swap(arr: var seq[uint32]; i, j: int) =
+  let tmp = arr[i]
+  arr[i] = arr[j]
+  arr[j] = tmp
+
+
+func pseudoAngle[F](dx, dy: F): F =
+  let p = dx / (dx.abs + dy.abs)
+  result = (if dy > 0: 3 - p else: 1 + p)
+
+
+func dist[F](ax, ay, bx, by: F): F =
+  let
+    dx = ax - bx
+    dy = ay - by
+  result = dx * dx + dy * dy
+
+
+func inCircle[F](ax, ay, bx, by, cx, cy, px, py: F): bool =
+  let
+    dx = ax - px
+    dy = ay - py
+    ex = bx - px
+    ey = by - py
+    fx = cx - px
+    fy = cy - py
+
+    ap = dx * dx + dy * dy
+    bp = ex * ex + ey * ey
+    cp = fx * fx + fy * fy
+
+    result = dx * (ey * cp - bp * fy) -
+             dy * (ex * cp - bp * fx) +
+             ap * (ex * fy - ey * fx) < 0
+
+
+func circumradius[F](ax, ay, bx, by, cx, cy: F): F =
+  let
+    dx = bx - ax
+    dy = by - ay
+    ex = cx - ax
+    ey = cy - ay
+
+    bl = dx * dx + dy * dy
+    cl = ex * ex + ey * ey
+    d = 0.5 / (dx * ey - dy * ex)
+
+    x = (ey * bl - dy * cl) * d
+    y = (dx * cl - ex * bl) * d
+
+  result = x * x + y * y
+
+
+func circumcenter[F](ax, ay, bx, by, cx, cy: F): tuple[x, y: F] =
+  let
+    dx = bx - ax
+    dy = by - ay
+    ex = cx - ax
+    ey = cy - ay
+
+    bl = dx * dx + dy * dy
+    cl = ex * ex + ey * ey
+    d = 0.5 / (dx * ey - dy * ex)
+
+    x = ax + (ey * bl - dy * cl) * d
+    y = ay + (dx * cl - ex * bl) * d
+
+  result = (x, y)
+
+
+proc quicksort[F](ids: var seq[uint32]; dists: seq[F]; left, right: int) =
+  if (right - left <= 20):
+    var i = left + 1
+    while i <= right:
+      let
+        temp = ids[i]
+        tempDist = dists[temp]
+      var j = i - 1
+      while j >= left and dists[ids[j]] > tempDist:
+        ids[j + 1] = ids[j]
+        dec j
+      ids[j + 1] = temp
+      inc i
+  else:
+    let median = ashr(left + right, 1)
+    var
+      i = left + 1
+      j = right
+    swap(ids, median, i)
+    if dists[ids[left]] > dists[ids[right]]: swap(ids, left, right)
+    if dists[ids[i]] > dists[ids[right]]: swap(ids, i, right)
+    if dists[ids[left]] > dists[ids[i]]: swap(ids, left, i)
+
+    let
+      temp = ids[i]
+      tempDist = dists[temp]
+    while true:
+      while true:
+        inc i
+        if dists[ids[i]] < tempDist: break
+      while true:
+        dec j
+        if dists[ids[j]] > tempDist: break
+      if j < i: break
+      swap(ids, i, j)
+    ids[left + 1] = ids[j]
+    ids[j] = temp
+
+    if right - i + 1 >= j - left:
+      quicksort(ids, dists, i, right)
+      quicksort(ids, dists, left, j - 1)
+    else:
+      quicksort(ids, dists, left, j - 1)
+      quicksort(ids, dists, i, right)
+
+
 proc update(this: var Delaunator) =
+
+  # TODO
+  proc u_hashKey() = discard
+  proc u_legalize() = discard
+  proc u_link() = discard
+  proc u_addTriangle() = discard
+
   this.triangles = this.d_triangles[1 .. ^1]
   discard
 
@@ -64,6 +187,7 @@ proc fromPoints*[P, T](points: seq[P]; getX: proc (p: P): T = defaultGetX; getY:
       coords[2 * i] = getX(point)
       coords[2 * i + 1] = getY(point)
   fromCoords(coords)
+
 
 
 when isMainModule:
