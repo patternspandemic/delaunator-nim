825d6d3b6e85caa19d7d53f33fa1531560fbbf19
patternspandemic <patternspandemic@live.com>
2023-05-16 10:35:47 -0700

    Add more helpers.

--- src/helpers.nim
+++ src/helpers.nim
@@ -1,96 +1,8 @@
 # TODO: inline things
 #       iterators sans ids?
 
-import std/sequtils, std/sugar
+import std/sequtils, std/sugar, std/sets
 
-#[
-
-function forEachTriangleEdge(points, delaunay, callback) {
-    for (let e = 0; e < delaunay.triangles.length; e++) {
-        if (e > delaunay.halfedges[e]) {
-            const p = points[delaunay.triangles[e]];
-            const q = points[delaunay.triangles[nextHalfedge(e)]];
-            callback(e, p, q);
-        }
-    }
-}
-
-function pointsOfTriangle(delaunay, t) {
-    return edgesOfTriangle(t)
-        .map(e => delaunay.triangles[e]);
-}
-
-function forEachTriangle(points, delaunay, callback) {
-    for (let t = 0; t < delaunay.triangles.length / 3; t++) {
-        callback(t, pointsOfTriangle(delaunay, t).map(p => points[p]));
-    }
-}
-
-function trianglesAdjacentToTriangle(delaunay, t) {
-    const adjacentTriangles = [];
-    for (const e of edgesOfTriangle(t)) {
-        const opposite = delaunay.halfedges[e];
-        if (opposite >= 0) {
-            adjacentTriangles.push(triangleOfEdge(opposite));
-        }
-    }
-    return adjacentTriangles;
-}
-
-function circumcenter(a, b, c) {
-    const ad = a[0] * a[0] + a[1] * a[1];
-    const bd = b[0] * b[0] + b[1] * b[1];
-    const cd = c[0] * c[0] + c[1] * c[1];
-    const D = 2 * (a[0] * (b[1] - c[1]) + b[0] * (c[1] - a[1]) + c[0] * (a[1] - b[1]));
-    return [
-        1 / D * (ad * (b[1] - c[1]) + bd * (c[1] - a[1]) + cd * (a[1] - b[1])),
-        1 / D * (ad * (c[0] - b[0]) + bd * (a[0] - c[0]) + cd * (b[0] - a[0])),
-    ];
-}
-
-function triangleCenter(points, delaunay, t) {
-    const vertices = pointsOfTriangle(delaunay, t).map(p => points[p]);
-    return circumcenter(vertices[0], vertices[1], vertices[2]);
-}
-
-function forEachVoronoiEdge(points, delaunay, callback) {
-    for (let e = 0; e < delaunay.triangles.length; e++) {
-        if (e < delaunay.halfedges[e]) {
-            const p = triangleCenter(points, delaunay, triangleOfEdge(e));
-            const q = triangleCenter(points, delaunay, triangleOfEdge(delaunay.halfedges[e]));
-            callback(e, p, q);
-        }
-    }
-}
-
-function edgesAroundPoint(delaunay, start) {
-    const result = [];
-    let incoming = start;
-    do {
-        result.push(incoming);
-        const outgoing = nextHalfedge(incoming);
-        incoming = delaunay.halfedges[outgoing];
-    } while (incoming !== -1 && incoming !== start);
-    return result;
-}
-
-function forEachVoronoiCell(points, delaunay, callback) {
-    const index = new Map(); // point id to half-edge id
-    for (let e = 0; e < delaunay.triangles.length; e++) {
-        const endpoint = delaunay.triangles[nextHalfedge(e)];
-        if (!index.has(endpoint) || delaunay.halfedges[e] === -1) {
-            index.set(endpoint, e);
-        }
-    }
-    for (let p = 0; p < points.length; p++) {
-        const incoming = index.get(p);
-        const edges = edgesAroundPoint(delaunay, incoming);
-        const triangles = edges.map(triangleOfEdge);
-        const vertices = triangles.map(t => triangleCenter(points, delaunay, t));
-        callback(p, vertices);
-    }
-}
-]#
 
 #[
 bounds
@@ -107,9 +19,9 @@ iterHullEdges
 - iterTriangles
 - triangleCircumenter
 - iterVoronoiEdges
-edgeIdsAroundPoint
-iterVoronoiRegions
-voronoiRegion
+- edgeIdsAroundPoint
+- iterVoronoiRegions
+- voronoiRegion
 
 midpoint
 perpBisectSlope
@@ -126,12 +38,12 @@ nearest site
 ]#
 
 
-func halfedgeIdsOfTriangle(t: int32): array[3, int32] =
+func halfedgeIdsOfTriangle*(t: int32): array[3, int32] =
   ## The halfedge ids of a triangle with id `t`.
   return [3 * t, 3 * t + 1, 3 * t + 2]
 
 
-func triangleIdOfEdge(e: int32): int32 =
+func triangleIdOfEdge*(e: int32): int32 =
   ## The id of the triangle for which halfedge with id `e` is a part. (also the id of the 1st point?)
   return floorDiv[int32](e, 3)
 
@@ -152,7 +64,7 @@ func prevHalfedge*(e: int32): int32 =
     return e - 1
 
 
-iterator iterTriangleEdges*(d: Delaunator): tuple[e: int32, p: array[2, SomeFloat], q: array[2, SomeFloat]] =
+iterator iterTriangleEdges*(d: Delaunator): tuple[e: int32; p, q: array[2, SomeFloat]] =
   ## Provides an iterator yielding values for each edge of the triangulation.
   ## The values yielded are the id of the halfedge chosen for the edge, an
   ## array describing the point the edge starts at, and an array describing
@@ -210,7 +122,7 @@ func triangleCircumcenter*(d: Delaunator, t: int32): array[2, SomeFLoat] =
     (cx, cy) = circumcenter(p1[0], p1[1], p2[0], p2[1], p3[0], p3[1])
   return [cx, cy]
 
-iterator iterVoronoiEdges*(d: Delaunator): tuple[e: int32, p, q: array[2, SomeFloat]] =
+iterator iterVoronoiEdges*(d: Delaunator): tuple[e: int32; p, q: array[2, SomeFloat]] =
   ## Provides an iterator yielding values for each bisecting voronoi edge of the
   ## graph dual to the triangulation. The values yielded are the id of the
   ## halfedge chosen for the bisected edge, an array describing the
@@ -229,3 +141,55 @@ iterator iterVoronoiEdges*(d: Delaunator): tuple[e: int32, p, q: array[2, SomeFl
       yield (e, p, q)
     if not e < d.triangles.len: break
     inc e
+
+
+func edgeIdsAroundPoint*(d: Delaunator, e: int32): seq[int32] =
+  ## The ids of all halfedges pointing to the point that halfedge `e` points to.
+  ## TODO: test whether this works for case when `e` points to a point on the
+  ## hull. Should now be okay with setting `start` to leftmost halfedge.
+  var
+    # Get the leftmost edge pointing to the point e is pointing to.
+    start = d.d_pointToLeftmostHalfedgeIndex[nextHalfedge(e)]
+    incomming = start
+    edgeIds = newSeqOfCap[int32](10) # TODO: Not sure what's a good starting capacity
+  while true:
+    edgeIds.add(incomming)
+    incomming = d.halfedges[nextHalfedge(incomming)]
+    if incomming == -1 or incomming == start: break
+  return edgeIds
+
+
+func iterVoronoiRegions*(d: Delaunator): tuple[p: uint32, verts: seq[array[2, SomeFLoat]]] =
+  ## Provides an iterator yielding values for each region of the voronoi diagram.
+  ## The values yielded are the id of the point to which the region belongs, and
+  ## a seq of vertices describing the region's polygon. Excluded by default
+  ## are infinite unbound regions. For such regions to be included, a bounding
+  ## region must be provided to clip these regions against.
+  ## TODO: Support voronoi regions of the hull. (via overloaded iterator?)
+  var
+    # Do not yield regions of hull points by default
+    seen = toHashSet(d.hull)
+    e = 0
+  while true:
+    let p = d.triangles[nextHalfedge(e)]
+    if not seen.contains(p):
+      seen.incl(p)
+      let
+        edgeIds = edgeIdsAroundPoint(d, e)
+        triangleIds = map(edgeIds, proc(h: int32): int32 = triangleIdOfEdge(h))
+        vertices = map(triangleIds, proc(t: int32): array[2, SomeFloat] = triangleCircumcenter(d, t))
+      yield (p, vertices)
+    if not e < d.triangles.len: break
+    inc e
+
+
+# FIXME: Does not provide clipped regions for points on hull
+#        Param p and shadowed p are the same?
+func voronoiRegion*(d: Delaunator, p: uint32): tuple[p: uint32, verts: seq[array[2, SomeFloat]]] =
+  let
+    incomming = d.d_pointToLeftmostHalfedgeIndex[p]
+    p = d.triangles[nextHalfedge(incomming)] # FIXME: prob remove
+    edgeIds = edgeIdsAroundPoint(d, incomming)
+    triangleIds = map(edgeIds, proc(h: int32): int32 = triangleIdOfEdge(h))
+    vertices = map(triangleIds, proc(t: int32): array[2, SomeFloat] = triangleCircumcenter(d, t))
+  return (p, vertices)
